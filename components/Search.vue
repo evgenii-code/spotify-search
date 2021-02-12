db81c73c2c519c9d059409e247fb171e24a5b187
evgenii-code <osipenkov.evgenii@gmail.com>
2021-02-12 13:35:39 +0300

    Search component styling

--- components/Search.vue
+++ components/Search.vue
@@ -1,11 +1,15 @@
 <template>
-    <v-card max-width="600" width="100%" outlined>
-        <v-container class="px-10 py-5">
+    <v-card max-width="900" width="100%" outlined>
+        <v-container class="px-5 px-sm-10 py-5">
             <validation-observer ref="observer" v-slot="{ invalid }" slim>
                 <form headline @submit.prevent="search">
-                    <v-list-item-title> Search on Spotify </v-list-item-title>
+                    <h1 class="text-sm-h3 text-lg-h1 font-weight-black">
+                        Search on Spotify
+                    </h1>
 
-                    <div class="mt-5 d-flex justify-space-between align-end">
+                    <div
+                        class="mt-5 d-flex flex-column flex-sm-row justify-space-between align-sm-end"
+                    >
                         <validation-provider
                             name="Search"
                             rules="required"
@@ -13,19 +17,22 @@
                         >
                             <v-text-field
                                 v-model="searchQuery"
-                                label="Search..."
+                                placeholder="Search..."
                                 hide-details="auto"
-                                class="mr-10"
+                                class="mr-0 mb-5 mr-sm-10 mb-sm-0"
+                                color="success"
                             />
                         </validation-provider>
 
                         <v-card-actions class="pa-0">
                             <v-btn
-                                outlined
                                 rounded
-                                text
                                 type="submit"
                                 :disabled="invalid"
+                                :loading="loading"
+                                depressed
+                                color="success"
+                                class="px-8"
                             >
                                 Search
                             </v-btn>
@@ -36,7 +43,7 @@
                         <v-chip-group
                             v-model="selectedTypesIds"
                             class="mt-5"
-                            active-class="primary--text"
+                            active-class="success--text"
                             column
                             mandatory
                             multiple
@@ -59,6 +66,7 @@
 export default {
     data() {
         return {
+            loading: false,
             selectedTypesIds: [],
             availableTypes: ['artist', 'album', 'track', 'playlist'],
             searchQuery: '',
@@ -71,9 +79,12 @@ export default {
                 (selectedTypeId) => this.availableTypes[selectedTypeId]
             );
 
+            this.loading = true;
+
             this.$axios
                 .get(`/search?query=${this.searchQuery}&type=${type}`)
-                .then(({ data }) => console.log('data', data));
+                .then(({ data }) => console.log('data', data))
+                .finally(() => (this.loading = false));
         },
     },
 };
