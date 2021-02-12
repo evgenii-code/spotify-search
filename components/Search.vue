4085077a0379327e3e12be3cb0a392fb6a24a4bb
evgenii-code <osipenkov.evgenii@gmail.com>
2021-02-12 13:03:22 +0300

    Search component

--- components/Search.vue
+++ components/Search.vue
@@ -1,19 +1,57 @@
 <template>
-    <v-card class="pa-6" max-width="600" width="100%" outlined>
-        <v-list-item-title class="headline mb-1">
-            Search on Spotify
-        </v-list-item-title>
+    <v-card max-width="600" width="100%" outlined>
+        <v-container class="px-10 py-5">
+            <validation-observer ref="observer" v-slot="{ invalid }" slim>
+                <form headline @submit.prevent="search">
+                    <v-list-item-title> Search on Spotify </v-list-item-title>
 
-        <v-text-field
-            v-model="searchQuery"
-            label="Search..."
-            :rules="rules"
-            hide-details="auto"
-        />
+                    <div class="mt-5 d-flex justify-space-between align-end">
+                        <validation-provider
+                            name="Search"
+                            rules="required"
+                            slim
+                        >
+                            <v-text-field
+                                v-model="searchQuery"
+                                label="Search..."
+                                hide-details="auto"
+                                class="mr-10"
+                            />
+                        </validation-provider>
 
-        <v-card-actions>
-            <v-btn outlined rounded text @click="search"> Search </v-btn>
-        </v-card-actions>
+                        <v-card-actions class="pa-0">
+                            <v-btn
+                                outlined
+                                rounded
+                                text
+                                type="submit"
+                                :disabled="invalid"
+                            >
+                                Search
+                            </v-btn>
+                        </v-card-actions>
+                    </div>
+
+                    <validation-provider name="Types" rules="required" slim>
+                        <v-chip-group
+                            v-model="selectedTypesIds"
+                            class="mt-5"
+                            active-class="primary--text"
+                            column
+                            mandatory
+                            multiple
+                        >
+                            <v-chip
+                                v-for="availableType in availableTypes"
+                                :key="availableType"
+                            >
+                                {{ availableType }}
+                            </v-chip>
+                        </v-chip-group>
+                    </validation-provider>
+                </form>
+            </validation-observer>
+        </v-container>
     </v-card>
 </template>
 
@@ -21,22 +59,20 @@
 export default {
     data() {
         return {
+            selectedTypesIds: [],
+            availableTypes: ['artist', 'album', 'track', 'playlist'],
             searchQuery: '',
-            rules: [
-                (value) => !!value || 'Required.',
-                (value) => (value && value.length >= 3) || 'Min 3 characters',
-            ],
         };
     },
 
     methods: {
         search() {
-            console.log('axios', this.$axios.defaults.headers);
+            const type = this.selectedTypesIds.map(
+                (selectedTypeId) => this.availableTypes[selectedTypeId]
+            );
 
             this.$axios
-                .get(
-                    `/search?query=${this.searchQuery}&type=album,artist,playlist,track`
-                )
+                .get(`/search?query=${this.searchQuery}&type=${type}`)
                 .then(({ data }) => console.log('data', data));
         },
     },
